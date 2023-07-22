82b80623d4efe686289a8bca50d47082a64dbb15
John Bogil <johncbogil@gmail.com>
2023-07-22 10:25:16 -0400

    open modal

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,14 +1,48 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 
 export const Map = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
   return (
     <div className="Map">
       <Box display="flex" justifyContent="center">
         <Button variant="contained" target="_" href="https://google.com">
           Collect For Your Neighborhood!
         </Button>
+        <Button variant="contained" color="primary" onClick={handleOpen}>
+          Open Modal
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <h2 id="modal-modal-title">Text in a modal</h2>
+            <p id="modal-modal-description">
+              Here is the content for your modal.
+            </p>
+            <Button variant="contained" color="secondary" onClick={handleClose}>
+              Close Modal
+            </Button>
+          </Box>
+        </Modal>
       </Box>
     </div>
   );
