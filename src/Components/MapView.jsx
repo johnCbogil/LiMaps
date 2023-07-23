1679853cb42a766ea9a7296dddf127b23fd92536
John Bogil <johncbogil@gmail.com>
2023-07-23 11:23:15 -0400

    map poc

--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
+import { Map, Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export const MapView = () => {
+  const landmarks = [
+    { name: 'Statue of Liberty', coordinates: { latitude: 40.6892, longitude: -74.0445 } },
+    { name: 'Central Park', coordinates: { latitude: 40.7851, longitude: -73.9683 } },
+    { name: 'Times Square', coordinates: { latitude: 40.7589, longitude: -73.9851 } },
+  ];
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -20,7 +27,27 @@ export const MapView = () => {
   };
   return (
     <div className="MapView">
-      <Box display="flex" justifyContent="center">
+    <Map
+        mapboxAccessToken="pk.eyJ1Ijoiam9obmNib2dpbCIsImEiOiJjbGtlNDFnM2kxMzk0M3JsczNpN2RrenhoIn0.41osgqUQ7EAVlfesFgIzJg"
+        initialViewState={{
+          longitude: -73.9060,
+          latitude: 40.7128,
+          zoom: 10.0,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      >
+            {landmarks.map((landmark, i) => (
+        <Marker
+          key={i}
+          longitude={landmark.coordinates.longitude}
+          latitude={landmark.coordinates.latitude}
+          onClick={() => setSelectedLandmark(landmark)} // Set selected landmark on click
+        >
+      <img src="src/pin.svg" />
+        </Marker>
+      ))}
+      </Map>
+      {/* <Box display="flex" justifyContent="center">
         <Button variant="contained" target="_" href="https://google.com">
           Collect For Your Neighborhood!
         </Button>
@@ -43,7 +70,7 @@ export const MapView = () => {
             </Button>
           </Box>
         </Modal>
-      </Box>
+      </Box> */}
     </div>
   );
 };
